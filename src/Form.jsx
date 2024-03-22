5ae691934caf7e365c33fdfad0e37064c70ee653
muriukiian <muriukiian47@gmail.com>
2024-03-23 00:29:15 +0300

    updated the css file

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,9 +3,9 @@ function Form(){
     <form className="form">
       <label>Sign in.</label><br></br>
       <label for="name">Username</label><br></br>
-      <input type="name" id="name" name="name" placeholder="Enter Username" /><br/>
+      <input class="input" type="name" id="name" name="name" placeholder="Enter Username" /><br/>
       <label for="password">Password</label><br></br>
-      <input type="password" id="passweord" name="password" placeholder="Enter password" required /><br></br>
+      <input class="input" type="password" id="passweord" name="password" placeholder="Enter password" required /><br></br>
       <button type="submit" className="button">Sign in</button>
     </form>
   );
